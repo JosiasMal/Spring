8972c98a23a11844b0cfe2691208fe5156011463
JosiasMal <144867933+JosiasMal@users.noreply.github.com>
2024-03-04 08:31:42 -0600

    Add files via upload

---
+++ spring/demo1/src/main/java/com/appwhitspring/demo1/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.appwhitspring.demo1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("details")
+    public String details(){
+        return "details";
+    }
+
+}
